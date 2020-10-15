9eb074f94d7b885c2e0ad55b0c08c353ed43cb74
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-10-15 17:10:59 +0800

    [MarsRover][Raymond] test: execute command L with given location and heading: 0,0,N

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -16,4 +16,16 @@ class MarsRoverTest {
         assertEquals(1,marsRover.getLocationY());
         assertEquals("N",marsRover.getHeading());
     }
+    @Test
+    void should_x_0_y_0_heading_N_when_execute_given_x_0_y_0_N_and_command_L() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        //when
+        marsRover.executeCommands("L");
+        //then
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(0,marsRover.getLocationY());
+        assertEquals("W",marsRover.getHeading());
+    }
+
 }
