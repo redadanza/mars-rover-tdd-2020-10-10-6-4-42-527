955243296d2858d1a2f9d7a76fb222be2f2611e0
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-10-15 18:43:07 +0800

    [MarsRover][Raymond] Feature: execute command MLMR with given location and heading: 0,0,W

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -137,6 +137,18 @@ class MarsRoverTest {
         assertEquals(0,marsRover.getLocationY());
         assertEquals("N",marsRover.getHeading());
     }
+    @Test
+    void should_x_n1_y_1_heading_N_when_execute_given_x_0_y_0_W_and_command_MLMR() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        //when
+        marsRover.executeCommands("MLMR");
+        //then
+        assertEquals(-1,marsRover.getLocationX());
+        assertEquals(1,marsRover.getLocationY());
+        assertEquals("N",marsRover.getHeading());
+    }
+
 
 
 }
